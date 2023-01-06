0fd285ccaccb49c43abccd2d075ecf985bf36a77
Erdi Salgın <110611268+Salginerdi@users.noreply.github.com>
2023-01-06 18:58:29 +0300

    JavaScript ile Faktöriyel Hesaplama Uygulaması
    JavaScript For Döngüsü ile Faktöriyel Hesaplama

---
+++ app.js
@@ -0,0 +1,10 @@
+// Faktoriyel Hesaplama
+
+let sayi = Number(prompt("Bir sayı giriniz!"));
+let carpim = 1;
+
+for (let i = 1; i <= sayi; i++) {
+  carpim = carpim * i;
+}
+
+alert("Sonuç : " + carpim);
